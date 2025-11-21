6d06b0bb2559fc97f1fea6d7a1a4324112f380a9
chongs01 <shuhei.chong@bbc.co.uk>
2025-11-21 10:29:09 +0000

    add tooling edp

--- leo_cli/gobbc/gobbc_cli.py
+++ leo_cli/gobbc/gobbc_cli.py
@@ -21,7 +21,7 @@ def refresh(env, project, duration):
     try:
         gobbc_api = Gobbc(env=env, project=project, duration=duration)
         account = gobbc_api.refresh_wormhole_credentials()
-        click.echo(f"Successfully refreshed AWS credentials to {env} {project} - {account}.")
+        click.echo(f"Successfully refreshed AWS credentials to {env} {project} - {account}. ")
     except Exception as ex:
         click.echo(f"Failed to refresh AWS credentials. {str(ex)}")
     finally:
